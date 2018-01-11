af78e4cd0aa8df3888589209853cf853b5569552
lassilehto <30998224+lassilehto@users.noreply.github.com>
2018-01-11 15:21:40 +0200

    Add files via upload

---
+++ First.py
@@ -0,0 +1,12 @@
+#%% DEFINES BLOCKS OF CODE
+import numpy as np
+import scipy
+import pylab as pl
+#%% THIS WILL OUTPUT SOME WISDOM
+print('Python and Lassi, a marriage made in heaven!')
+#%%
+a = np.random.random([10,10])
+print(a)
+#%%
+pl.imshow(a)
+
